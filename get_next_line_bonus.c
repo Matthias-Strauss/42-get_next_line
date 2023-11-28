9606043bb10d5c8420c8e1e79ffc8211f3da0629
mstrauss <matthias.strauss@proton.me>
2023-11-28 18:36:03 +0100

    bonus finished

---
+++ get_next_line_bonus.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstrauss <mstrauss@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 18:34:34 by mstrauss          #+#    #+#             */
+/*   Updated: 2023/11/28 18:34:39 by mstrauss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+char	*ft_clean_up(char *tmp, char *buffer, char *final, int nl_index)
+{
+	ft_strlcpy(buffer, &tmp[nl_index + 1], BUFFER_SIZE);
+	ft_strlcpy(final, tmp, nl_index + 2);
+	return (final);
+}
+
+char	*ft_get_more(char *tmp, char *buffer, int fd)
+{
+	char	*new;
+	int		bytes_read;
+
+	bytes_read = 1;
+	if (tmp == NULL)
+		tmp = ft_calloc(1, sizeof(char));
+	if (tmp == NULL)
+		return (NULL);
+	new = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (new == NULL)
+		return (free(tmp), NULL);
+	while (bytes_read && ft_strchr(tmp, '\n') == -1)
+	{
+		bytes_read = read(fd, new, BUFFER_SIZE);
+		if (bytes_read == -1)
+			return (ft_bzero(buffer, BUFFER_SIZE), ft_bzero(new, BUFFER_SIZE),
+				free(new), NULL);
+		new[bytes_read] = '\0';
+		tmp = ft_strjoin(tmp, new);
+		if (tmp == NULL)
+			return (free(new), free(tmp), NULL);
+		if (ft_strlen(tmp) == 0)
+			return (free(new), free(tmp), NULL);
+	}
+	return (free(new), tmp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[1024][BUFFER_SIZE + 1];
+	char		*tmp;
+	char		*final;
+	int			nl_index;
+
+	if (fd > 1023 || fd < 0)
+		return (NULL);
+	if (read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
+		return (ft_bzero(buffer[fd], BUFFER_SIZE + 1), NULL);
+	tmp = ft_strdup(buffer[fd]);
+	if (tmp == NULL)
+		return (NULL);
+	ft_bzero(buffer[fd], BUFFER_SIZE);
+	tmp = ft_get_more(tmp, buffer[fd], fd);
+	if (tmp == NULL)
+		return (NULL);
+	nl_index = ft_strchr(tmp, '\n');
+	if ((nl_index == 0 && tmp[0] != '\n') || nl_index == -1)
+		return (tmp);
+	final = ft_calloc(nl_index + 2, sizeof(char));
+	if (final == NULL)
+		return (free(tmp), NULL);
+	ft_clean_up(tmp, buffer[fd], final, nl_index);
+	return (free(tmp), final);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstrauss <mstrauss@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 23:42:35 by mstrauss          #+#    #+#             */
+/*   Updated: 2023/11/27 16:02:06 by mstrauss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+void	*ft_calloc(size_t count, size_t size);
+size_t	ft_strlcat(char *dst, char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+char	*ft_strdup(char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(const char *s);
+int		ft_strchr(char *s, int c);
+void	ft_bzero(void *s, size_t n);
+
+#endif
